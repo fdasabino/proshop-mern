4832f05eb35fb0b7b91be82bdb67717f06457689
Francisco Sabino <fdasabino@gmail.com>
2022-07-12 19:33:17 +0200

    i nodemon and concurrently to run both ends

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
+import axios from "axios";
 import { Row, Col, ListGroup, Image, Card, Button, ListGroupItem } from "react-bootstrap";
 import Rating from "../components/Rating";
-import products from "../products";
 
 const ProductScreen = () => {
+  const [product, setProduct] = useState({});
   const params = useParams();
-  const product = products.find((p) => p._id === params.id);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${params.id}`);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [params.id]);
+
   return (
     <>
       <Link className="btn btn-secondary my-3" to="/">
