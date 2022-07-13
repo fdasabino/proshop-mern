8f29113236ca394026bd7203c3cc359628b654a0
Francisco Sabino <fdasabino@gmail.com>
2022-07-14 01:18:56 +0200

    added cart to store and started with cart screen

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -1,6 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Link, useParams, useLocation } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Row, Col, ListGroup, ListGroupItem, Image, Form, Button, Card } from "react-bootstrap";
+import Message from "../components/Message";
+import { addToCart } from "../redux-actions/cartActions";
 
 const CartScreen = () => {
+  const { id } = useParams();
+  const productId = id;
+
+  const location = useLocation();
+  const qty = new URLSearchParams(location.search).get("qty");
+
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
   return <div>CartScreen</div>;
 };
 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,12 +3,22 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import { combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { productListReducer, productDetailsReducer } from "./redux-reducers/productReducers";
+import { cartReducer } from "./redux-reducers/cartReducers";
 
-const initialState = {};
+const cartItemsFromStorage = localStorage.getItem("cartItems")
+  ? JSON.parse(localStorage.getItem("cartItems"))
+  : [];
+
+const initialState = {
+  cart: {
+    cartItems: cartItemsFromStorage,
+  },
+};
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 
 const middleware = [thunk];
