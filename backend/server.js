b51080b0c0ff9fbefe2152636ccaff0a475919f2
Francisco Sabino <fdasabino@gmail.com>
2022-07-13 14:01:55 +0200

    implemented basic error handling to backend

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
 const app = express();
 dotenv.config();
 
 connectDB();
 
+//Routes
+app.use("/api/products", productRoutes);
+
+// error handlers
+app.use(errorHandler);
+app.use(notFound);
+
 app.get("/", (req, res) => {
   res.send("API is running...");
-});
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
 });
 
 app.listen(
