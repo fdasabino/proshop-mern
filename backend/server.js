370bcc9d2ff412dd08e037ded8213d1419e48044
Francisco Sabino <fdasabino@gmail.com>
2022-07-17 14:59:00 +0200

    implemented json web token to user backend

--- backend/server.js
+++ backend/server.js
@@ -2,28 +2,30 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 
-const app = express();
 dotenv.config();
-
 connectDB();
+const app = express();
+app.use(express.json());
 
 //Routes
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+
+//API entrypoint
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 // error handlers
 app.use(errorHandler);
 app.use(notFound);
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
+// PORT
+const PORT = process.env.PORT || 5000;
 app.listen(
-  process.env.PORT,
-  console.log(
-    `Server Running in ${process.env.NODE_ENV.bgMagenta} mode, on port ${process.env.PORT}`.blue
-      .underline.bold
-  )
+  PORT,
+  console.log(`Server Running in ${PORT.bgMagenta} mode, on port ${PORT}`.blue.underline.bold)
 );
